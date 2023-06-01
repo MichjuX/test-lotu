798dc95b75f33801bc62a859a154499a8f5f0ca7
MichjuX <mks2003a@gmail.com>
2023-06-01 21:55:06 +0200

    Section selfPaint WORKS BUT NOT XD (still trying to fix it)

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -48,16 +48,16 @@ public class MyPanel extends JPanel implements ActionListener {
         Graphics2D g2D = (Graphics2D) g;
         g2D.setPaint(new Color(255, 255, 255));
         g2D.drawRect(airShip.currentPos.x, airShip.currentPos.y, 100, 100);
-        for(Section section : airShip.sections){
-            g2D.drawLine(section.startPoint.x, section.startPoint.y, section.endPoint.x, section.endPoint.y);
-        }
-//        g2D.drawLine(600, 600, 600, 500);
-//        g2D.drawLine(600, 500, 700, 500);
-//        g2D.drawLine(700, 500, 700, 100);
-//        g2D.drawLine(700, 100, 800, 100);
-//        g2D.drawLine(800, 100, 800, 500);
-//        g2D.drawLine(800, 500, 900, 500);
-//        g2D.drawLine(900, 500, 900, 600);
+//        for(Section section : airShip.sections){
+//            g2D.drawLine(section.startPoint.x, section.startPoint.y, section.endPoint.x, section.endPoint.y);
+//        }
+        g2D.drawLine(600, 600, 600, 500);
+        g2D.drawLine(600, 500, 700, 500);
+        g2D.drawLine(700, 500, 700, 100);
+        g2D.drawLine(700, 100, 800, 100);
+        g2D.drawLine(800, 100, 800, 500);
+        g2D.drawLine(800, 500, 900, 500);
+        g2D.drawLine(900, 500, 900, 600);
     }
     int counter=0;
     @Override
