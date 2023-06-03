9bf58b5f5eb2cc589581cf01827d0817fdfe3092
MichjuX <mks2003a@gmail.com>
2023-06-03 02:20:54 +0200

    Section selfPaint WORKS!!!!

--- src/AirShip.java
+++ src/AirShip.java
@@ -23,31 +23,19 @@ public class AirShip extends JPanel implements ActionListener{
         this.currentPos = startPos;
     }
     AirShip(Point startPos){
-//        sections.add(new Section(0,
-//                new Point(RND(1280),RND(720)),
-//                new Point(RND(1280),RND(720)),
-//
-//                ));
-//        for(int i=0; i<3; i++){
-//            sections.add(new Section())
-//        }
         sections.add(new Section(0,
                 startPos,
                 new Point(RND(1280),RND(720)),
                 5
         ));
-
-//        sections.add(new Section(sections.get(0).endTime,
-//                sections.get(0).endPoint,
-//                new Point(700,500),
-//                2
-//        ));
+//         kutas
+//        sections.add(new Section(sections.get(0).endTime, sections.get(0).endPoint, new Point(700,500), 2));
 //        sections.add(new Section(sections.get(1).endTime, sections.get(1).endPoint, new Point(700,100), 2));
 //        sections.add(new Section(sections.get(2).endTime, sections.get(2).endPoint, new Point(800,100), 2));
 //        sections.add(new Section(sections.get(3).endTime, sections.get(3).endPoint, new Point(800,500), 2));
 //        sections.add(new Section(sections.get(4).endTime, sections.get(4).endPoint, new Point(900,500), 2));
 //        sections.add(new Section(sections.get(5).endTime, sections.get(5).endPoint, new Point(900,600), 2));
-        for(int i=0; i<1; i++){
+        for(int i=0; i<5; i++){
             sections.add(new Section(sections.get(i).endTime,
                     sections.get(i).endPoint,
                     new Point(RND(1280),RND(720)),
@@ -94,25 +82,31 @@ public class AirShip extends JPanel implements ActionListener{
     }
     int currentTime = 0;
     int i = 0;
+    public void paint(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.setPaint(new Color(255, 255, 255));
+        g2D.drawLine(900, 500, 900, 600);
+    }
     public void actionPerformed(ActionEvent event){
-        if(i<sections.size()) {
-            if (sections.get(i).endTime < currentTime) {
-                i++;
-            }
+        if(i<=sections.size()-1) {
 
             if (i-1 >= sections.size()) {
                 return;
             }
-            Section tempsection = sections.get(0);
-            tempsection = sections.get(i);
+            Section tempsection = sections.get(i);
 
     //        System.out.println("SEKCJA "+i+"\n");
     //        System.out.println(tempsection.toString());;
             double ratio = ((currentTime - tempsection.startTime) / (double) (tempsection.endTime - tempsection.startTime));
             currentPos.x = (int) (tempsection.x + ratio * tempsection.distanceX) -50;
             currentPos.y = (int) (tempsection.y + ratio * tempsection.distanceY) -50;
-    //        System.out.println(currentPos.x + " " + currentPos.y);
+            System.out.println(currentPos.x + " " + currentPos.y);
             currentTime++;
+            if (sections.get(i).endTime <= currentTime) {
+                i++;
+            }
         }
+        Line line = new Line();
     }
 }
--- src/Line.java
+++ src/Line.java
@@ -1,8 +1,11 @@
 import java.awt.*;
+import javax.swing.*;
 
-public class Line {
-    public void drawLine(Graphics2D g2D, Section startPoint, Section endPoint){
+public class Line extends JPanel{
+    public void paint(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2D = (Graphics2D) g;
         g2D.setPaint(new Color(255, 255, 255));
-        g2D.drawLine(startPoint.x,startPoint.y,endPoint.x,endPoint.y);
+        g2D.drawLine(900, 500, 900, 600);
     }
 }
