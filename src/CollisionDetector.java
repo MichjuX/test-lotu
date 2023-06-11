57c9c45d8ce60f40b0639d28146009f49d3fd821
Kacper Przezdziecki <kprzezdziecki0@gmail.com>
2023-06-11 15:16:58 +0200

    Drzewa na kwadraty

--- src/CollisionDetector.java
+++ src/CollisionDetector.java
@@ -1,16 +1,18 @@
 import java.util.HashMap;
 import java.util.List;
+
 public class CollisionDetector {
     private List<StationaryObject> stationaryObjects;
     private List<AirShip> airShips;
-    public CollisionDetector(List<StationaryObject> stationaryObjects, List<AirShip> airShips)
-    {
+
+    public CollisionDetector(List<StationaryObject> stationaryObjects, List<AirShip> airShips) {
         this.stationaryObjects = stationaryObjects;
         this.airShips = airShips;
     }
+
     //prawdopodobnie tutaj jest spierdolone vvvv
-    public boolean isColliding(StationaryObject stationaryObject,AirShip airShip) {
-        double stationaryObjectLeft = stationaryObject.position.x - (double)stationaryObject.getWidth() / 2;
+    public boolean isColliding(StationaryObject stationaryObject, AirShip airShip) {
+        /*double stationaryObjectLeft = stationaryObject.position.x - (double)stationaryObject.getWidth() / 2;
         double stationaryObjectRight = stationaryObject.position.x + (double)stationaryObject.getWidth() / 2;
         double stationaryObjectTop = stationaryObject.position.y - (double)stationaryObject.getHeight() / 2;
         double stationaryObjectBottom = stationaryObject.position.y + (double)stationaryObject.getHeight() / 2;
@@ -25,14 +27,29 @@ public class CollisionDetector {
         return (stationaryObjectRight > airShipLeft
                 && stationaryObjectLeft < airShipRight
                 && stationaryObjectBottom > airShipTop
-                && stationaryObjectTop < airShipBottom);
+                && stationaryObjectTop < airShipBottom);*/
+        double stationaryObjectCenterX = stationaryObject.position.x + (((double) stationaryObject.parametr) / 2);
+        double stationaryObjectCenterY = stationaryObject.position.y + (((double) stationaryObject.parametr) / 2);
+        double stationaryObjectHalfDiag = Math.sqrt(((double) stationaryObject.parametr / 2) * ((double) stationaryObject.parametr / 2)
+                + ((double) stationaryObject.parametr / 2) * ((double) stationaryObject.parametr / 2));
+
+        double airShipCenterX = airShip.currentPos.x;
+        double airShipCenterY = airShip.currentPos.y;
+        double airShipHalfDiag = Math.sqrt((double) ((airShip.getAirShipHeight() * airShip.getAirShipHeight()) / 4)
+                + ((double) (airShip.getAirshipWidth() * airShip.getAirshipWidth()) / 4));
+        double correctDistance = stationaryObjectHalfDiag + airShipHalfDiag;
+        double distance = Math.sqrt((Math.abs(stationaryObjectCenterX - airShipCenterX) * Math.abs(stationaryObjectCenterX - airShipCenterX)))
+                + (Math.abs(stationaryObjectCenterY - airShipCenterY) * Math.abs(stationaryObjectCenterY - airShipCenterY));
+        return correctDistance >= distance;
     }
+
     public void detectCollisions() {
         for (StationaryObject stationaryObject : stationaryObjects) {
             for (AirShip airShip : airShips) {
                 boolean isColliding = isColliding(stationaryObject, airShip);
-                stationaryObject.setColliding(isColliding);
                 airShip.setColliding(isColliding);
+                stationaryObject.setColliding(isColliding);
+
             }
         }
     }
