dc9ae7e5d03f818b312c0411d42b74b2b9bb1c40
Kacper Przezdziecki <kprzezdziecki0@gmail.com>
2023-06-14 01:35:50 +0200

    znikanie po zakończeniu

--- src/AirShip.java
+++ src/AirShip.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class AirShip extends JPanel implements ActionListener{
+abstract public class AirShip extends JPanel implements ActionListener{
     Point startPos;
     Point currentPos;
     List<Section> sections = new ArrayList<>();
